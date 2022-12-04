315edaa165953a52d3366eea8076929fc535f964
Yu Bin <subenmoom50@gmail.com>
2022-12-04 19:55:13 +0900

    📖 :: How to run deno program

---
+++ study/first.ts
@@ -0,0 +1,11 @@
+console.log("Welcome to Deno!")
+
+/*
+How to run the program from the turminal : deno run first.ts
+
+Or
+
+You can scripts from URLs. deno host a library of example code, one of example to print hello world
+
+deno run https://deno.land/std@0.103.0/examples/welcome.ts
+*/
