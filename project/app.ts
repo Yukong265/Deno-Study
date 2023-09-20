457f42a426aeec018f7eabe67a238aa088b479c3
Yukong265 <subenmoom50@gmail.com>
2023-09-20 15:19:42 +0900

    post 읽기

--- project/app.ts
+++ project/app.ts
@@ -1,16 +1,61 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts"
+import {
+  Application,
+  Router,
+  helpers,
+  Context,
+} from "https://deno.land/x/oak/mod.ts";
+import client from "./db/client.ts";
+
+
+const { getQuery } = helpers;
 
 const app = new Application();
 const router = new Router();
-const port = 8000
+const port = 8000;
 
-app.use(router.routes())
-app.use(router.allowedMethods())
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-router.get("/", (ctx) => {
-    ctx.response.body =  "Hello World!"
-})
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
 
-console.log(`server running on port ${port}`)
+router
+  .get("/post", async (ctx: Context) => {
+    ctx.response.body = await client.query(`SELECT * FROM post`);
+  })
+  .post("/post", async (ctx: Context) => {
+    const body = await ctx.request.body();
+    console.log(body.value)
+    ctx.response.body = body.value
+    //ctx.response.body = await client.execute(`INSERT TO post(title, content) values(?)`, [body.title, body.content]);
+  });
 
-await app.listen({ port:8000 })
+router
+  .get("/users/:id", async (ctx: Context) => {
+    const { id } = getQuery(ctx, { mergeParams: true });
+  })
+  .get("/users/email/:email", async (ctx: Context) => {
+    const { email } = getQuery(ctx, { mergeParams: true });
+  })
+  .get("/users/:id/address", async (ctx: Context) => {
+    const { id } = getQuery(ctx, { mergeParams: true });
+  })
+  .post("/users", async (ctx: Context) => {
+    const body = ctx.request.body();
+    const user = await body.value;
+  })
+  .post("/users/:id/address", async (ctx: Context) => {
+    const { id } = getQuery(ctx, { mergeParams: true });
+    const body = ctx.request.body();
+    const address = await body.value;
+  })
+  .delete("/users/:id", async (ctx: Context) => {
+    const { id } = getQuery(ctx, { mergeParams: true });
+  });
+
+console.log(`server running on port ${port}`);
+
+await app.listen({ port: 8000 });
---
+++ project/interface/post.ts
@@ -0,0 +1,5 @@
+export default interface post {
+    id?: number,
+    title: string,
+    content:string,
+}
